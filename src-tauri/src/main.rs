01c47eb069bfb21d1ee5a9be20451ff67373cc60
Vinicius Sales <vinicius.2010.s@gmail.com>
2022-01-27 17:07:34 -0300

    remove user preferences from rust

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,19 +5,15 @@
 
 mod error;
 mod redis;
-mod user_preferences;
 use crate::redis::test_connection;
-use crate::user_preferences::{get_user_preferences, save_user_preferences};
 use tauri_plugin_window_state::WindowState;
 
 fn main() {
   tauri::Builder::default()
-    .plugin(WindowState::default())
     .invoke_handler(tauri::generate_handler![
       test_connection,
-      get_user_preferences,
-      save_user_preferences,
     ])
+    .plugin(WindowState::default())
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
